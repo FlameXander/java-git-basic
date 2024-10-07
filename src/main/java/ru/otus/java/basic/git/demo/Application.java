062185fcbb6458e15b2a88866e6618ceeb8cb1f8
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 20:59:40 +0300

    Фикс отрисовки

--- src/main/java/ru/otus/java/basic/git/demo/Application.java
+++ src/main/java/ru/otus/java/basic/git/demo/Application.java
@@ -7,9 +7,11 @@ import java.nio.charset.StandardCharsets;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(1);
+        System.out.println("---");
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(2);
+        System.out.println("---");
     }
 
     public static void doSomething() {
