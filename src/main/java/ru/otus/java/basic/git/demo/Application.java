1249bbab321b38f6a96a70a56f481459dbbb96b1
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 21:30:30 +0300

    Фикс четвертой двойки

--- src/main/java/ru/otus/java/basic/git/demo/Application.java
+++ src/main/java/ru/otus/java/basic/git/demo/Application.java
@@ -33,7 +33,7 @@ public class Application {
         System.out.println(1);
         System.out.println(1);
         System.out.println(1);
-        System.out.println(1);
+        System.out.println(2);
         System.out.println(1);
     }
 }
