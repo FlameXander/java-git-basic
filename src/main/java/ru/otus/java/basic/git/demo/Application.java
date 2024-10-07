0db91a8106ddbb634e8d7542b04f3745dd98f45b
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 20:19:45 +0300

    Научились выводить числа в консоль

--- src/main/java/ru/otus/java/basic/git/demo/Application.java
+++ src/main/java/ru/otus/java/basic/git/demo/Application.java
@@ -3,5 +3,8 @@ package ru.otus.java.basic.git.demo;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
