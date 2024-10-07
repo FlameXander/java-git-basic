1694715c888a8387b97b9b086d6af315c0f08794
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 20:47:59 +0300

    Возможность сохранять файлы

--- src/main/java/ru/otus/java/basic/git/demo/Application.java
+++ src/main/java/ru/otus/java/basic/git/demo/Application.java
@@ -1,5 +1,9 @@
 package ru.otus.java.basic.git.demo;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -13,5 +17,13 @@ public class Application {
         System.out.println("- -");
         System.out.println("- -");
         System.out.println("---");
+    }
+
+    public static void saveToFile() {
+        try (FileOutputStream out = new FileOutputStream("1.txt")) {
+            out.write("Hello World".getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
